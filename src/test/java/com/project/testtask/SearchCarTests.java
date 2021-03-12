8e4fa358b0727951b6cc3078773486cdafa872b7
Michael Podoprigora <52321995+MikePodoprigora@users.noreply.github.com>
2021-03-11 23:21:11 -0500

    Search car form added

--- src/test/java/com/project/testtask/SearchCarTests.java
+++ src/test/java/com/project/testtask/SearchCarTests.java
@@ -3,6 +3,7 @@ package com.project.testtask;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -15,28 +16,51 @@ public class SearchCarTests {
         wd.manage().window().maximize();
         goToSite("https://www.mobile.de/");
 
+
+        //To create Login form tests
+
+
     }
 
     @Test
-    public void testCar(){
+    public void testCar() {
         click(By.cssSelector(".gicon-flag-ger-s"));
         click(By.cssSelector("[href='https://www.mobile.de/?lang=en']"));
 
+        if (!isElementPresent(By.cssSelector("#qs-categories__CarsTab"))) {
+            click(By.cssSelector("#qs-categories__CarsTab"));
+        }
 
+// fill Car search form
+        click(By.cssSelector("#qsmakeBuy"));
+        click(By.cssSelector("[value='25200']"));
+        click(By.cssSelector("#qsmodelBuy"));
+        click(By.cssSelector("[value='36']"));
+        click(By.cssSelector("#qssub"));     // Search Button
     }
 
 
-//    public boolean isElementPresent(By locator) {
-//        return wd.findElements(locator).size() > 0;
-//    }
 
-    private void click(By locator) {
+
+    @AfterClass(enabled = false)
+    public void tearDown(){
+        wd.quit();
+    }
+
+
+
+    public void click(By locator) {
         wd.findElement(locator).click();
     }
 
 
-    private void goToSite(String url) {
+    public void goToSite(String url) {
         wd.navigate().to(url);
     }
+
+    public boolean isElementPresent(By locator) {
+        return wd.findElements(locator).size() > 0;
+    }
+
 
 }
